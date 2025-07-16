8747fc0671a4bb6f73bd7986d85e851b4626d894
Kirill Madorin <madorinkv@gmail.com>
2025-07-16 18:19:55 +0200

    build process updated

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+const path = require("path");
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// This is the crucial part for monorepo support with Prisma
+	experimental: {
+		outputFileTracingRoot: path.join(__dirname, "../../"),
+	},
 };
 
 export default nextConfig;
